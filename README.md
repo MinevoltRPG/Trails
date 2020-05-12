92166efc9f5afbc4b8c6edd083f4c69267669a6d
James Lomax <jameslomax96@gmail.com>
2020-05-12 13:33:54 +0100

    Update to minecraft version 1.15.2 + minor improvements to code and README file.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Trails
-Trails is a great RPG plugin that works on the premise that, in the real world, walking wears down trails and paths from dirt to stone, eventually.
+<h1 align="center"> Trails ✨ Spigot plugin </h1> <br>
+
+<p align="center">
+   [![Preview video](http://img.youtube.com/vi/L9XmBr-IAeA/0.jpg)](http://www.youtube.com/watch?v=L9XmBr-IAeA "Trails Plugin Spigot")
+</p>
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [How to start](#how-to-start)
+- [License](#license)
+
+## Introduction
+
+Trails is a great RPG plugin that works on the premise that, in the real world, walking wears down trails and paths from dirt to stone, eventually. Trails aims to add this element to Minecraft by automatically creating trails, paths, and roads based on the use of a trail. The trail contents and wear passes are fully configurable, unlocking the possibilities for a number of different trails or uses on your server.
+
+## How to start
+1. Install Spigot on http://www.spigotmc.org/
+2. Download this plugin at https://www.spigotmc.org/resources/villagernames-%E2%9C%A8-lightweight-villager-update-1-14-1-15.76618/
+3. Drop this plugin in your server's "/plugins" folder
+
+## License
+This project is licensed under the MIT license - see the [LICENSE.md](LICENSE.md) file for details.
--- src/main/java/me/ccrama/Trails/listeners/MoveEventListener.java
+++ src/main/java/me/ccrama/Trails/listeners/MoveEventListener.java
@@ -14,88 +14,97 @@ import me.ccrama.Trails.objects.TrailBlock;
 import me.ccrama.Trails.objects.WrappedLocation;
 import me.drkmatr1984.customevents.moveEvents.SignificantPlayerMoveEvent;
 
-public class MoveEventListener implements Listener {
-	
-	private Trails main;
-	
-	private Links links;
-	
-	public MoveEventListener(Trails plugin) {
-		this.main = plugin;
-	    this.links = this.main.getConfigManager().getLinks();
-	}
-	
-	@EventHandler
-	public void walk(SignificantPlayerMoveEvent e) {
-		Player p = e.getPlayer();
-		if(main.isToggledOff(p)) {
-			return;
-		}
-		// Check towny conditions
-		if(main.getTownyHook()!=null) {
-			if(main.getTownyHook().isWilderness(p) && !main.getConfigManager().isPathsInWilderness()) {
-				return;
-			}
-			if(main.getConfigManager().isTownyPathsPerm()) {
-				if(main.getTownyHook().isInHomeNation(p) && !main.getTownyHook().hasNationPermission(p) && !main.getTownyHook().isInHomeTown(p)) {
-					return;
-				}
-				if(main.getTownyHook().isInHomeTown(p) && !main.getTownyHook().hasTownPermission(p)) {
-					return;
-				}
-			}		
-		}
-		// Check worldguard conditions
-		if(main.getWorldGuardHook()!=null && !main.getWorldGuardHook().canBuild(p, p.getLocation().subtract(0.0D, 1.0D, 0.0D)))	
-			return;          		
-		makePath(e.getFrom().subtract(0.0D, 1.0D, 0.0D).getBlock());
-		//log blocks
-	}
-	
-	@SuppressWarnings("deprecation")
-	private void makePath(Block block) {
-		Material type = block.getType();
-	    byte dataValue = block.getData();
-	    for(Link link : this.links){
-	    	if(link.getMat() == type && link.getDataValue() == dataValue){
-	    		double foo = Math.random() * 100.0D;
-	            if(foo <= (double)link.chanceOccurance()) {
-	            	for(TrailBlock b : this.main.getBlockDataManager().getTrailBlocks()){
-	            		if(b.getWrappedLocation().isLocation(block.getLocation())){
-	            			int walked = b.getWalks();
-	           			 	if(walked >= link.decayNumber()){
-	           			 		this.main.getBlockDataManager().removeTrailBlock(b);
-	           			 		this.changeNext(block);
-	           			 		return;
-	           			 	}else{
-	           			 		this.main.getBlockDataManager().removeTrailBlock(b);
-	           			 		this.main.getBlockDataManager().addTrailBlock(new TrailBlock(b.getWrappedLocation(),(walked+1)));
-	           			 		return;
-	           			 	}
-	           		 	}
-	           	 	}
-	            	this.main.getBlockDataManager().walkedOver.add(new TrailBlock(new WrappedLocation(block.getLocation()), 1));
-	            }
-	      	}
-	    }  	
-	}
+import java.lang.reflect.InvocationTargetException;
 
-	@SuppressWarnings("deprecation")
-	private void changeNext(Block block) {
-		Material type = block.getType();
-		byte dataValue = block.getData();
-	    for(Link link : links){
-	       	if(link.getMat() == type && link.getDataValue() == dataValue){
-	       		if(link.getNext() != null) {
-	       	         Material nextMat = link.getNext().getMat();
-	       	         byte dValue = link.getNext().getDataValue();
-	       	         block.setType(nextMat);
-	       	         block.setData(dValue);
-	       	         block.getState().update(true);
-	       		}
-	       	}
-	    }     
-	}
-	
+public class MoveEventListener implements Listener {
+
+    private Trails main;
+
+    private Links links;
+
+    public MoveEventListener(Trails plugin) {
+        this.main = plugin;
+        this.links = this.main.getConfigManager().getLinks();
+    }
+
+    @EventHandler
+    public void walk(SignificantPlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        if (main.isToggledOff(p)) {
+            return;
+        }
+        // Check towny conditions
+        if (main.getTownyHook() != null) {
+            if (main.getTownyHook().isWilderness(p) && !main.getConfigManager().isPathsInWilderness()) {
+                return;
+            }
+            if (main.getConfigManager().isTownyPathsPerm()) {
+                if (main.getTownyHook().isInHomeNation(p) && !main.getTownyHook().hasNationPermission(p) && !main.getTownyHook().isInHomeTown(p)) {
+                    return;
+                }
+                if (main.getTownyHook().isInHomeTown(p) && !main.getTownyHook().hasTownPermission(p)) {
+                    return;
+                }
+            }
+        }
+        // Check worldguard conditions
+        if (main.getWorldGuardHook() != null && !main.getWorldGuardHook().canBuild(p, p.getLocation().subtract(0.0D, 1.0D, 0.0D)))
+            return;
+        makePath(e.getFrom().subtract(0.0D, 1.0D, 0.0D).getBlock());
+        //log blocks
+    }
+
+    @SuppressWarnings("deprecation")
+    private void makePath(Block block) {
+        Material type = block.getType();
+        byte dataValue = block.getData();
+        for (Link link : this.links) {
+            if (link.getMat() == type && link.getDataValue() == dataValue) {
+                double foo = Math.random() * 100.0D;
+                if (foo <= (double) link.chanceOccurance()) {
+                    for (TrailBlock b : this.main.getBlockDataManager().getTrailBlocks()) {
+                        if (b.getWrappedLocation().isLocation(block.getLocation())) {
+                            int walked = b.getWalks();
+                            if (walked >= link.decayNumber()) {
+                                this.main.getBlockDataManager().removeTrailBlock(b);
+                                try {
+                                    this.changeNext(block);
+                                } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                                    e.printStackTrace();
+                                }
+                                return;
+                            } else {
+                                this.main.getBlockDataManager().removeTrailBlock(b);
+                                this.main.getBlockDataManager().addTrailBlock(new TrailBlock(b.getWrappedLocation(), (walked + 1)));
+                                return;
+                            }
+                        }
+                    }
+                    this.main.getBlockDataManager().walkedOver.add(new TrailBlock(new WrappedLocation(block.getLocation()), 1));
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void changeNext(Block block) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Material type = block.getType();
+        byte dataValue = block.getData();
+        for (Link link : links) {
+            if (link.getMat() == type && link.getDataValue() == dataValue) {
+                if (link.getNext() != null) {
+                    Material nextMat = link.getNext().getMat();
+                    byte dValue = link.getNext().getDataValue();
+                    block.setType(nextMat);
+                    // depreciated as of 1.12 onwards
+                    // block.setData(dValue);
+                    // j10max update to 1.15.2 [12/05/2020]
+                    block.getState().setRawData(dValue);
+                    block.getState().update(true);
+                }
+            }
+        }
+    }
+
 }
 	
